f3d320f96bafd3d8f603b5d83118cfb3cf6f1fbf
zarrlock <desnod@gmail.com>
2021-01-10 16:18:30 +0100

    Add files via upload

---
+++ shopping-list.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : Dim 10 jan. 2021 à 15:13
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `shopping-list`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+DROP TABLE IF EXISTS `categorie`;
+CREATE TABLE IF NOT EXISTS `categorie` (
+  `idCat` int(11) NOT NULL AUTO_INCREMENT,
+  `nomCat` varchar(255) NOT NULL,
+  PRIMARY KEY (`idCat`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `categorie`
+--
+
+INSERT INTO `categorie` (`idCat`, `nomCat`) VALUES
+(1, 'Fruit'),
+(2, 'Légume'),
+(3, 'Viande'),
+(4, 'Ustensile ');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `magasin`
+--
+
+DROP TABLE IF EXISTS `magasin`;
+CREATE TABLE IF NOT EXISTS `magasin` (
+  `idMag` int(11) NOT NULL AUTO_INCREMENT,
+  `nomMagasin` varchar(255) NOT NULL,
+  PRIMARY KEY (`idMag`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `magasin`
+--
+
+INSERT INTO `magasin` (`idMag`, `nomMagasin`) VALUES
+(1, 'Boucher'),
+(2, 'Delhaize');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `mesure`
+--
+
+DROP TABLE IF EXISTS `mesure`;
+CREATE TABLE IF NOT EXISTS `mesure` (
+  `idMes` int(11) NOT NULL AUTO_INCREMENT,
+  `nomMes` varchar(255) NOT NULL,
+  PRIMARY KEY (`idMes`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `mesure`
+--
+
+INSERT INTO `mesure` (`idMes`, `nomMes`) VALUES
+(1, 'KG'),
+(2, 'Littre'),
+(3, 'Unité');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produit`
+--
+
+DROP TABLE IF EXISTS `produit`;
+CREATE TABLE IF NOT EXISTS `produit` (
+  `idProd` int(11) NOT NULL AUTO_INCREMENT,
+  `nomProd` varchar(255) NOT NULL,
+  `idCat` int(11) NOT NULL,
+  PRIMARY KEY (`idProd`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
