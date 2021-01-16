67567a7d72f8db77ca8ca1224000fe13f37d3ca8
Emile Cyimena <cyimena09@hotmail.com>
2021-01-16 09:01:11 +0100

    create DAOFactory

---
+++ src/main/java/be/ifosup/dao/DAOFactory.java
@@ -0,0 +1,54 @@
+package be.ifosup.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DAOFactory {
+    // ATTRIBUTES
+    private final String url;
+    private final String userName;
+    private final String password;
+
+    // CONSTRUCTOR
+    public DAOFactory(String url, String userName, String password){
+        this.url = url;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    // METHODS
+    // Load driver
+    public static DAOFactory getInstance(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        DAOFactory instance = new DAOFactory("jdbc:mysql://localhost:3306/poo?serverTimezone=CET", "root", "");
+        return instance;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, userName, password);
+    }
+
+    // Get Dao for sql tables
+    // décommenter lorsque les classes auront été créé
+//    public MagasinDAO getMagasinDAO(){
+//        return new UserDAOImpl(this);
+//    }
+//
+//    public PanierDAO getPanierDAO(){
+//        return new TodoDAOImpl(this);
+//    }
+//
+//    public ProduitDAO getProduitDAO() { return new CategoryDAOImpl(this); }
+//
+//    public CategorieDAO getCategorieDAO(){
+//        return new TodoDAOImpl(this);
+//    }
+//
+//    public MesureDAO getMesureDAO() { return new CategoryDAOImpl(this); }
+}
