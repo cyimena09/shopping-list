2b20a624115b1a179c0b7bc013a7ecfe5771a1c7
Benoit <benoit.vankoningsloo@bvkl.be>
2021-01-30 09:42:23 +0100

    liaisons fk entre tables voir wiki pour schéma

---
+++ dump-shopping-list-202101300941.sql
@@ -0,0 +1,161 @@
+-- MySQL dump 10.18  Distrib 10.3.27-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: shopping-list
+-- ------------------------------------------------------
+-- Server version	10.3.27-MariaDB-0+deb10u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categorie`
+--
+
+DROP TABLE IF EXISTS `categorie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categorie` (
+  `idCategorie` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  PRIMARY KEY (`idCategorie`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categorie`
+--
+
+LOCK TABLES `categorie` WRITE;
+/*!40000 ALTER TABLE `categorie` DISABLE KEYS */;
+INSERT INTO `categorie` VALUES (1,'Fruit'),(2,'Légume'),(3,'Viande'),(4,'Ustensile ');
+/*!40000 ALTER TABLE `categorie` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `magasin`
+--
+
+DROP TABLE IF EXISTS `magasin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `magasin` (
+  `idMagasin` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`idMagasin`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `magasin`
+--
+
+LOCK TABLES `magasin` WRITE;
+/*!40000 ALTER TABLE `magasin` DISABLE KEYS */;
+INSERT INTO `magasin` VALUES (1,'Boucher'),(2,'Delhaize');
+/*!40000 ALTER TABLE `magasin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mesure`
+--
+
+DROP TABLE IF EXISTS `mesure`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mesure` (
+  `idMesure` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  PRIMARY KEY (`idMesure`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mesure`
+--
+
+LOCK TABLES `mesure` WRITE;
+/*!40000 ALTER TABLE `mesure` DISABLE KEYS */;
+INSERT INTO `mesure` VALUES (1,'KG'),(2,'Littre'),(3,'Unité');
+/*!40000 ALTER TABLE `mesure` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `panier`
+--
+
+DROP TABLE IF EXISTS `panier`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `panier` (
+  `idPanier` int(11) NOT NULL AUTO_INCREMENT,
+  `idMagasin` int(11) NOT NULL,
+  `idProduit` int(11) NOT NULL,
+  `quantite` int(11) NOT NULL,
+  PRIMARY KEY (`idPanier`),
+  KEY `produit_FK` (`idProduit`),
+  KEY `magasin_FK` (`idMagasin`),
+  CONSTRAINT `magasin_FK` FOREIGN KEY (`idMagasin`) REFERENCES `magasin` (`idMagasin`),
+  CONSTRAINT `produit_FK` FOREIGN KEY (`idProduit`) REFERENCES `produit` (`idProduit`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `panier`
+--
+
+LOCK TABLES `panier` WRITE;
+/*!40000 ALTER TABLE `panier` DISABLE KEYS */;
+/*!40000 ALTER TABLE `panier` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `produit`
+--
+
+DROP TABLE IF EXISTS `produit`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `produit` (
+  `idProduit` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `idCategorie` int(11) DEFAULT NULL,
+  `idMesure` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idProduit`),
+  KEY `categorie_FK` (`idCategorie`),
+  KEY `mesure_FK` (`idMesure`),
+  CONSTRAINT `categorie_FK` FOREIGN KEY (`idCategorie`) REFERENCES `categorie` (`idCategorie`),
+  CONSTRAINT `mesure_FK` FOREIGN KEY (`idMesure`) REFERENCES `mesure` (`idMesure`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produit`
+--
+
+LOCK TABLES `produit` WRITE;
+/*!40000 ALTER TABLE `produit` DISABLE KEYS */;
+/*!40000 ALTER TABLE `produit` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'shopping-list'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-01-30  9:41:02
