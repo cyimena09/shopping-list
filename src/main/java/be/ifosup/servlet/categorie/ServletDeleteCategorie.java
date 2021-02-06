cfc977b1187138ede34207f4cb1f977231e376fc
Benoit <benoit.vankoningsloo@bvkl.be>
2021-02-06 12:26:15 +0100

    categorie

---
+++ src/main/java/be/ifosup/servlet/categorie/ServletDeleteCategorie.java
@@ -0,0 +1,40 @@
+package be.ifosup.servlet.categorie;
+
+import be.ifosup.dao.DAOFactory;
+import be.ifosup.categorie.Categorie;
+import be.ifosup.categorie.CategorieDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet(name = "ServletDeleteMesure", urlPatterns = "/delete_mesure")
+public class ServletDeleteCategorie extends HttpServlet {
+    // ATTRIBUTS
+    private CategorieDAO mesureDAO;
+
+    // METHODES
+    public void init(){
+        DAOFactory daoFactory = DAOFactory.getInstance();
+        this.categorieDAO = daoFactory.getCategorieDAO();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Récupération de l'id de la catégorie à supprimer.
+        String idMesure = request.getParameter("idMesure");
+
+        try {
+            // On appelle la méthode delete.
+            categorieDAO.deleteCategorie(Integer.parseInt(idMesure));
+            request.setAttribute("catégories", CategorieDAO.getCategories();
+        } catch (SQLException throwable){
+            throwable.printStackTrace();
+        }
+        request.getRequestDispatcher("views/mesure/mesures.jsp").forward(request, response);
+    }
+
+}
---
+++ src/main/java/be/ifosup/servlet/categorie/ServletUpdateCategorie.java
@@ -0,0 +1,54 @@
+package be.ifosup.servlet.categorie;
+
+import be.ifosup.dao.DAOFactory;
+import be.ifosup.categorie.Categorie;
+import be.ifosup.categorie.CategorieDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet(name = "ServletUpdateCategorie", urlPatterns = "/update_categorie")
+public class ServletUpdateCategorie extends HttpServlet {
+    private CategorieDAO mesureDAO;
+
+    public void init() {
+        DAOFactory daoFactory = DAOFactory.getInstance();
+        this.categorieDAO = daoFactory.getCategorieDAO();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Force UTF-8
+        request.setCharacterEncoding("UTF-8");
+        // Récupération depuis l'url et conversion en Integer de l'id de la catégorie.
+        String strIdCategorie = request.getParameter("idCategorie");
+        Integer idCategorie = Integer.parseInt(strIdCategorie);
+        // Récupération du nom de la catégorie depuis le formulaire.
+        String nomCategorie = request.getParameter("nomCategorie");
+
+        try {
+            CategorieDAO.updateCategorie(idCategorie, new Categorie(null, nomCategorie));
+            request.setAttribute("mesures", mesureDAO.getCategories());
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+        request.getRequestDispatcher("views/categorie/mesures.jsp").forward(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String strIdMesure = request.getParameter("idCategorie");
+
+        try {
+            // Ajout de la mesure dans l'attribut.
+            request.setAttribute("mesure", mesureDAO.getCategorieById();
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+        request.getRequestDispatcher("views/mesure/update_mesure.jsp").forward(request, response);
+    }
+
+}
